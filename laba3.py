07db5c8ebb7a690fe175fc89212f228de7ec0622
MariaRubtsova <124884461+MariaRubtsova@users.noreply.github.com>
2023-05-21 23:38:17 +0300

    Add files via upload

---
+++ laba3.py
@@ -0,0 +1,39 @@
+def zad1():
+    words = []
+    # программа останавливается при вводе слова "stop"
+    while (new_word := str(input())) != "stop":
+        words.append(new_word)
+
+    print(" ".join(words))
+
+def zad2():
+    words = []
+    # программа останавливается при вводе слова "stop"
+    while (new_word := str(input())) != "stop":
+        if "ф" in new_word or "Ф" in new_word:
+            print("Ого! Это редкое слово!")
+        else:
+            print("Эх, это не очень редкое слово")
+
+def zad3():
+    from random import randint
+
+    print("Для завершения игры введите слово stop")
+    while True:
+        a = randint(1, 100)
+        b = randint(1, 100)
+        print(f"{a} + {b} = ", end="")
+        res = input()
+        # проверка ввода
+        while not res.isdigit() and res != "stop":
+            print("Вы ввели что-то не то, повторите ввод: ", end="")
+            res = input()
+        if res == "stop":
+            break
+        res = int(res)
+        if a + b == res:
+            print("Правильно!")
+        else:
+            print("Ответ неправильный :(")
+
+zad3()
